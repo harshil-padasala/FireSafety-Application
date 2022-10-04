3fa29d9292abd1e4feb2d5e61d180edf8b2375b1
DESKTOP-M4QI8VT\HENIL <henilmistry74496@gmail.com>
2022-10-04 10:16:45 +0530

    Added carousel in main activity, with adapter and new resoursefile

---
+++ app/src/main/java/com/example/firesafety/CarousalAdapter.java
@@ -0,0 +1,49 @@
+package com.example.firesafety;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.List;
+
+public class CarousalAdapter extends PagerAdapter {
+
+    // list that will store the id of images...
+    List<Integer> list;
+
+    public CarousalAdapter(List<Integer> list) {
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view==object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View view = LayoutInflater.from(container.getContext()).inflate(R.layout.carousal_layout, container, false);
+        ImageView image = view.findViewById(R.id.carousal_image);
+
+        image.setImageResource(list.get(position));
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View)object);
+    }
+}
--- app/src/main/java/com/example/firesafety/MainActivity.java
+++ app/src/main/java/com/example/firesafety/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.firesafety;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager carousal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        carousal = findViewById(R.id.viewPager_carousal);
+
+        List<Integer> image_list = new ArrayList<>();
+        image_list.add(R.drawable.p1);
+        image_list.add(R.drawable.p2);
+        image_list.add(R.drawable.p3);
+        CarousalAdapter adapter = new CarousalAdapter(image_list);
+
+        carousal.setAdapter(adapter);
+
     }
 }
